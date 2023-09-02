f00c2d054a484e4849d90b370b929238d940fb06
Data-poeple-codes <datapeople34@gmail.com>
2023-09-02 16:24:55 +0600

    started

---
+++ static_site/app/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    // design navbar
+    return <nav className=" bg-slate-800 h-4 flex justify-between">
+        {/* logo */}
+        <ul>
+            <li>Courses</li>
+            <li>Instructor</li>
+            <li>Sign Up</li>
+        </ul>
+    </nav>
+};
+
+export default Header;
