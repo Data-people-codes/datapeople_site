55aac3cf905b061ff35ee5e000b12a5f6af98702
Fahim Muntasir <niloyf3@gmail.com>
2023-09-03 14:32:41 +0600

    navbar added

--- static_site/app/page.tsx
+++ static_site/app/page.tsx
@@ -1,12 +1,9 @@
-import RootLayout from "./layout"
-
 export default function Home() {
   return (
-    <RootLayout>
-      <main className=" flex flex-row justify-center">
+    <main className=" flex flex-row justify-center">
       <h1 className=" text-2xl">This is Data People</h1>
     </main>
-    </RootLayout>
+
     
   )
 }
--- static_site/tailwind.config.ts
+++ static_site/tailwind.config.ts
@@ -8,11 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      // custom color
+      colors:{
+        'atom_bg':'#292d34',
+        'atom_red': '#c16266',
+        'atom_green': '	#90b061',
+        'atom_green_light':'#9bbf50',
+        'atom_orange': '#be8a59'
+      }
     },
   },
   plugins: [],
