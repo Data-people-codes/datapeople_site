30cf973df9f6487f5bb46e665ab875144fb6ea84
Data-poeple-codes <datapeople34@gmail.com>
2023-09-13 15:49:38 +0600

    ki je korsi

--- static_site/app/components/Course.tsx
+++ static_site/app/components/Course.tsx
@@ -1,24 +1,25 @@
+
 import Image from "next/image";
 
 // course component
 
 function Course({ CourseName, CourseDesc, src }: { CourseName: string, CourseDesc: string, src: string }) {
   return <>
-    <div className=" mt-6 flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-      <div className=" p-6">
-        <Image
-          src={src}
-          alt=""
-          width={75}
-          height={75}
-        />
-      </div>
-      <div>
-        <h1 className=" mb-2 px-6 block font-sans text-2xl font-bold leading-snug tracking-normal text-blue-gray-900 antialiased">{CourseName}</h1>
-        <p className=" block px-6 pb-5 font-sans text-base font-normal leading-relaxed text-inherit antialiased">{CourseDesc}</p>
-      </div>
+      <div className=" mt-6 flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <div className=" p-6">
+          <Image
+            src={src}
+            alt=""
+            width={75}
+            height={75}
+          />
+        </div>
+        <div>
+          <h1 className=" mb-2 px-6 block text-2xl font-bold leading-snug tracking-normal text-blue-gray-900 antialiased">{CourseName}</h1>
+          <p className=" block px-6 pb-5 text-base font-normal leading-relaxed text-inherit antialiased">{CourseDesc}</p>
+        </div>
 
-    </div>
+      </div>
 
   </>
 }
--- static_site/app/page.tsx
+++ static_site/app/page.tsx
@@ -1,82 +1,90 @@
 import Image from "next/image"
 import Course from "./components/Course"
-import { IBM_Plex_Sans } from "next/font/google"
+import { IBM_Plex_Sans, JetBrains_Mono } from "next/font/google"
 
-const ibm_plex_sans = IBM_Plex_Sans({
+export const jbm = JetBrains_Mono({
   subsets:['latin'],
-  weight:['400','700']
+  weight: ['400', '700']
+})
+
+export const ibm_plex_sans = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700']
 })
 
 export default function Home() {
   return (
-    <div className={ibm_plex_sans.className}>
-    
+    <div className={jbm.className}>
 
-    <main className=" justify-center bg-atom_bg min-h-full text-white font-medium">
 
-      {/* Intro Section */}
-      <section id="intro" className=" flex flex-row flex-wrap justify-center lg:justify-around content-center items-center px-6 py-12 md:p-24 text-center lg:text-left">
+      <main className=" justify-center bg-atom_bg min-h-full text-white font-medium">
 
-        <div className=" w-2/3">
-          <h1 className=" text-orange-300 text-6xl font-bold sm:text-8xl pb-12">Data People</h1>
-          <div className=" pt-4 pb-8">
-            <h2 className=" text-4xl pb-4">From <span className=" text-atom_red font-bold">No Code</span> to <span className=" underline underline-offset-4 text-datacamp_green font-bold">MLOPS</span>.</h2>
-            <p className=" text-xl font-normal">Unlock the power of data and AI by learning <br />Python, ChatGPT, SQL, Power BI, and more.</p>
+        {/* Intro Section */}
+        <section id="intro" className=" flex flex-row flex-wrap justify-center lg:justify-around content-center items-center px-6 py-12 md:p-24 text-center lg:text-left">
+
+
+          <div className=" w-2/3 pb-12">
+            <div className={jbm.className}>
+              <h2 className=" text-4xl pb-2">From <span className="font-bold">No Code</span> to <span className=" underline underline-offset-4 font-bold">MLOPS</span>.</h2>
+              <h1 className=" text-6xl font-bold sm:text-8xl">Data People</h1>
+              <div className="pb-8">
+                <p className=" text-xl font-normal">Unlock the power of data and AI by learning <br />Python, ChatGPT, SQL, Power BI, and more.</p>
+              </div>
+            </div>
+
+            <div className=" flex items-center">
+              <button className=" inline-flex items-center mx-3 border-2 border-white outline-white hover:border-transparent hover:bg-white text-white hover:text-atom_bg py-2 px-10 rounded-sm">
+                Browse Courses
+              </button>
+
+              <button className=" inline-flex items-center mx-3 bg-white text-atom_bg hover:bg-datacamp_green py-2 px-10 rounded-sm">
+                Register Now
+              </button>
+            </div>
           </div>
 
-          <div className=" flex items-center">
-            <button className=" inline-flex items-center mx-3 border-2 border-white outline-white hover:border-transparent hover:bg-white text-white hover:text-atom_bg py-2 px-10 rounded-sm">
-              Browse Courses
-            </button>
-
-            <button className=" inline-flex items-center mx-3 bg-white text-atom_bg hover:bg-datacamp_green py-2 px-10 rounded-sm">
-              Register Now
-            </button>
+          <div className=" w-1/3 hidden lg:block">
+            <Image
+              src="/memory_brain.png"
+              alt="memory"
+              width={400}
+              height={400}
+            />
           </div>
-        </div>
-
-        <div className=" w-1/3 hidden lg:block">
-          <Image
-            src="/memory_brain.png"
-            alt="memory"
-            width={400}
-            height={400}
-          />
-        </div>
-      </section>
+        </section>
 
 
-      {/* Course Section */}
-      <section id="courses" className=" pt-6 px-20">
-        <h1 className=" text-2xl md:text-4xl text-center p-16"> <span className=" underline underline-offset-8 text-4xl md:text-6xl text-datacamp_green">Project-based</span> Live Classes</h1>
+        {/* Course Section */}
+        <section id="courses" className=" pt-6 px-20">
+          <h1 className=" text-2xl md:text-4xl text-center p-16"> <span className=" underline underline-offset-8 text-4xl md:text-6xl text-datacamp_green">Project-based</span> Live Classes</h1>
 
-        <div className=" flex flex-row flex-wrap justify-around content-center items-center">
+          <div className=" flex flex-row flex-wrap justify-around content-center items-center">
 
-          <Course CourseName="Noob to Pythonista" CourseDesc="Python A-Z, Learn Python from the best" src="/python-logo.svg" />
+            <Course CourseName="Noob to Pythonista" CourseDesc="Python A-Z, Learn Python from the best" src="/python-logo.svg" />
 
-          <Course CourseName="ML Crash Course" CourseDesc="Everything about Machine Learning, hands on with projects" src="/machine-learning-svgrepo-com.svg" />
+            <Course CourseName="ML Crash Course" CourseDesc="Everything about Machine Learning, hands on with projects" src="/machine-learning-svgrepo-com.svg" />
 
-          <Course CourseName="DL Crash Course" CourseDesc="Everything about Deep Learning, hands on with projects" src="/deep-learning-svgrepo-com.svg" />
+            <Course CourseName="DL Crash Course" CourseDesc="Everything about Deep Learning, hands on with projects" src="/deep-learning-svgrepo-com.svg" />
 
-          <Course CourseName="MLOPS" CourseDesc="Apply ML to Production! Just a click away!" src="/production-concept-svgrepo-com.svg" />
+            <Course CourseName="MLOPS" CourseDesc="Apply ML to Production! Just a click away!" src="/production-concept-svgrepo-com.svg" />
 
-          <Course CourseName="Project Pro" CourseDesc="Want to enhance portfolio? We got 50+ curated projects just for you" src="/github-svgrepo-com.svg" />
+            <Course CourseName="Project Pro" CourseDesc="Want to enhance portfolio? We got 50+ curated projects just for you" src="/github-svgrepo-com.svg" />
 
-          <Course CourseName="Research Publication" CourseDesc="Looking for a research group to learn and publish your work? " src="/experiment-svgrepo-com.svg" />
+            <Course CourseName="Research Publication" CourseDesc="Looking for a research group to learn and publish your work? " src="/experiment-svgrepo-com.svg" />
 
-          <Course CourseName="Expert Solution" CourseDesc="Looking for an expert for enterprise solution? Drop a message." src="/boss-svgrepo-com.svg" />
-        </div>
-      </section>
-
-
-      {/* Seminer */}
-      <section id="seminer" className=" pt-6 px-20">
-      <h1 className=" text-2xl md:text-4xl text-center p-16"> <span className=" underline underline-offset-8 text-4xl md:text-6xl text-datacamp_green">Free Seminer</span> Each Month</h1>
-
-      </section>
+            <Course CourseName="Expert Solution" CourseDesc="Looking for an expert for enterprise solution? Drop a message." src="/boss-svgrepo-com.svg" />
+          </div>
+        </section>
 
 
-    </main>
+        {/* Seminer */}
+        <section id="seminer" className=" pt-6 px-20">
+          <h1 className=" text-2xl md:text-4xl text-center p-16"> <span className=" underline underline-offset-8 text-4xl md:text-6xl text-datacamp_green">Free Seminer</span> Each Month</h1>
+
+        </section>
+
+
+      </main>
     </div>
 
   )
