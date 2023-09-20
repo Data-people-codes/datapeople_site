34f777ccece2e58d730510494b3098d7cd70c78e
Fahim Muntasir <niloyf3@gmail.com>
2023-09-20 22:34:32 +0600

    footer added

--- static_site/app/components/Footer.tsx
+++ static_site/app/components/Footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
     return <>
         <footer className=" bg-slate-950 text-slate-200">
-            <div className=" flex flex-col md:flex-row justify-between items-start p-8 md:p-20">
+            <div className=" flex flex-col md:flex-row justify-between items-start p-8 md:p-24">
                 <div className=" w-full md:w-1/3">
                     <h1 className="text-5xl  font-bold">Data People</h1>
                     <p className=" pt-8 text-slate-400">Data People was created by Nurul Akter Towhid with an aim to make AI, Data Science and Machine Learning accessible to everyone. Initially it started as an ed-tech, which later proceeded on becoming a fully enterprise AI solution and research center.</p>
@@ -31,9 +31,15 @@ export default function Footer() {
                             </div>
                         </a>
 
-                        <a href="https://www.facebook.com/natowhidpage">
+                        <a href="https://www.youtube.com/@DSwithTowhid">
                             <div className=" fill-slate-200 hover:fill-datacamp_green">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 576 512"><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" /></svg>
+                            </div>
+                        </a>
+
+                        <a href="https://www.youtube.com/@AIPodcastwithTowhid">
+                            <div className=" fill-slate-200 hover:fill-datacamp_green">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512"><path d="M319.4 372c48.5-31.3 80.6-85.9 80.6-148c0-97.2-78.8-176-176-176S48 126.8 48 224c0 62.1 32.1 116.6 80.6 148c1.2 17.3 4 38 7.2 57.1l.2 1C56 395.8 0 316.5 0 224C0 100.3 100.3 0 224 0S448 100.3 448 224c0 92.5-56 171.9-136 206.1l.2-1.1c3.1-19.2 6-39.8 7.2-57zm-2.3-38.1c-1.6-5.7-3.9-11.1-7-16.2c-5.8-9.7-13.5-17-21.9-22.4c19.5-17.6 31.8-43 31.8-71.3c0-53-43-96-96-96s-96 43-96 96c0 28.3 12.3 53.8 31.8 71.3c-8.4 5.4-16.1 12.7-21.9 22.4c-3.1 5.1-5.4 10.5-7 16.2C99.8 307.5 80 268 80 224c0-79.5 64.5-144 144-144s144 64.5 144 144c0 44-19.8 83.5-50.9 109.9zM224 312c32.9 0 64 8.6 64 43.8c0 33-12.9 104.1-20.6 132.9c-5.1 19-24.5 23.4-43.4 23.4s-38.2-4.4-43.4-23.4c-7.8-28.5-20.6-99.7-20.6-132.8c0-35.1 31.1-43.8 64-43.8zm0-144a56 56 0 1 1 0 112 56 56 0 1 1 0-112z"/></svg>
                             </div>
                         </a>
                         </div>
